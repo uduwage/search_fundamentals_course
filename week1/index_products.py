4846a1d41f4ab6445884b4b4770352ef5a9cc154
Anuradha Uduwage <uduw0001@umn.edu>
2023-02-12 19:47:32 +0000

    adding opensearch connection string

--- week1/index_products.py
+++ week1/index_products.py
@@ -85,7 +85,15 @@ def get_opensearch():
     port = 9200
     auth = ('admin', 'admin')
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    client = OpenSearch(
+        hosts=[{'host':host, 'port':port}],
+        http_compress=True,
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False,
+    )
     return client
 
 
